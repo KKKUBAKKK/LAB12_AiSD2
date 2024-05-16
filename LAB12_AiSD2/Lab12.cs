a59d775bd796aad93b0b0f990a30493cfe692c13
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-05-16 10:47:54 +0200

    Initial commit

---
+++ LAB12_AiSD2/Lab12.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASD
+{
+    public class WaterCalculator : MarshalByRefObject
+    {
+
+        /*
+         * Metoda sprawdza, czy przechodząc p1->p2->p3 skręcamy w lewo 
+         * (jeżeli idziemy prosto, zwracany jest fałsz).
+         */
+        private bool leftTurn(Point p1, Point p2, Point p3)
+        {
+            Point w1 = new Point(p2.x - p1.x, p2.y - p1.y);
+            Point w2 = new Point(p3.x - p2.x, p3.y - p2.y);
+            double vectProduct = w1.x * w2.y - w2.x * w1.y;
+            return vectProduct > 0;
+        }
+
+
+        /*
+         * Metoda wyznacza punkt na odcinku p1-p2 o zadanej współrzędnej y.
+         * Jeżeli taki punkt nie istnieje (bo cały odcinek jest wyżej lub niżej), zgłaszany jest wyjątek ArgumentException.
+         */
+        private Point getPointAtY(Point p1, Point p2, double y)
+        {
+            if (p1.y != p2.y)
+            {
+                double newX = p1.x + (p2.x - p1.x) * (y - p1.y) / (p2.y - p1.y);
+                if ((newX - p1.x) * (newX - p2.x) > 0)
+                    throw new ArgumentException("Odcinek p1-p2 nie zawiera punktu o zadanej współrzędnej y!");
+                return new Point(p1.x + (p2.x - p1.x) * (y - p1.y) / (p2.y - p1.y), y);
+            }
+            else
+            {
+                if (p1.y != y)
+                    throw new ArgumentException("Odcinek p1-p2 nie zawiera punktu o zadanej współrzędnej y!");
+                return new Point((p1.x + p2.x) / 2, y);
+            }
+        }
+
+
+        /// <summary>
+        /// Funkcja zwraca tablice t taką, że t[i] jest głębokością, na jakiej znajduje się punkt points[i].
+        /// 
+        /// Przyjmujemy, że pierwszy punkt z tablicy points jest lewym krańcem, a ostatni - prawym krańcem łańcucha górskiego.
+        /// </summary>
+        public double[] PointDepths(Point[] points)
+        {
+            List<(int s, int e, double l)> waterLevels = new List<(int s, int e, double l)> ();
+            int s = 0;
+            int m = 0;
+            int e = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (s >= m)
+                {
+                    if (points[i].y >= points[s].y)
+                        s = i;
+                    else
+                        m = i;
+                }
+                else if (m > e)
+                {
+                    if (points[i].y <= points[m].y)
+                        m = i;
+                    else
+                        e = i;
+                }
+                else
+                {
+                    if (points[e].y < points[i].y)
+                        e = i;
+                    else
+                    {
+                        waterLevels.Add((s, e, Math.Min(points[s].y, points[e].y)));
+                        s = i;
+                        m = i;
+                        e = i;
+                    }
+                }
+            }
+            
+            (int s, int e, double l)
+            
+            return null;
+        }
+        
+        /// <summary>
+        /// Funkcja zwraca objętość wody, jaka zatrzyma się w górach.
+        /// 
+        /// Przyjmujemy, że pierwszy punkt z tablicy points jest lewym krańcem, a ostatni - prawym krańcem łańcucha górskiego.
+        /// </summary>
+        public double WaterVolume(Point[] points)
+        {
+            return -1;
+        }
+    }
+
+    [Serializable]
+    public struct Point
+    {
+        public double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
